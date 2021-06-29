b643972739f10d1570bf32f18d832d9026655e3e
Pranav Vashi <neobuddy89@gmail.com>
2021-06-29 10:25:32 +0530

    Dumpster: Alert Slider: Add toggle to disable notifications [2/2]
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: Ic752dd6d683fe3fef1188550632e76b667f65e34

--- src/com/bootleggers/dumpster/fragments/NotificationsSettings.java
+++ src/com/bootleggers/dumpster/fragments/NotificationsSettings.java
@@ -10,6 +10,7 @@ import android.content.DialogInterface.OnClickListener;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.UserHandle;
+import android.content.res.Resources;
 import androidx.preference.ListPreference;
 import androidx.preference.SwitchPreference;
 import androidx.preference.Preference;
@@ -26,11 +27,26 @@ import com.android.settings.R;
 public class NotificationsSettings extends SettingsPreferenceFragment 
         implements OnPreferenceChangeListener {
 
+    private static final String ALERT_SLIDER_PREF = "alert_slider_notifications";
+
+    private Preference mAlertSlider;
+
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
 
         addPreferencesFromResource(R.xml.bootleg_dumpster_frag_notifications);
+
+        final PreferenceScreen prefScreen = getPreferenceScreen();
+        final Context mContext = getActivity().getApplicationContext();
+        final ContentResolver resolver = mContext.getContentResolver();
+        final Resources res = mContext.getResources();
+
+        mAlertSlider = (Preference) prefScreen.findPreference(ALERT_SLIDER_PREF);
+        boolean mAlertSliderAvailable = res.getBoolean(
+                com.android.internal.R.bool.config_hasAlertSlider);
+        if (!mAlertSliderAvailable)
+            prefScreen.removePreference(mAlertSlider);
     }
 
     @Override
